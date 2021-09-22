5ddbe4136a29a93f4de733a294ea0eecee61f9fa
perio29 <tokuzou10yohji@gmail.com>
2021-09-22 13:51:38 +0900

    redux-toolkit-1

---
+++ src/features/fetch/Fetch.js
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchAsyncGet, selectUsers } from "./fetchSlice";
+
+export const Fetch = () => {
+  const dispatch = useDispatch();
+  const users = useSelector(selectUsers);
+  useEffect(() => {
+    dispatch(fetchAsyncGet());
+  }, [dispatch]);
+  return (
+    <div>
+      {users.map((user) => (
+        <div key={user.id}>{user.email}</div>
+      ))}
+    </div>
+  );
+};
